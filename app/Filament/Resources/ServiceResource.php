370037a2607a6cf0c6660c3c9c160deac308c02b
cenksen <cenksen0605@gmail.com>
2023-11-10 19:02:51 +0300

    service crud completed

---
+++ app/Filament/Resources/ServiceResource.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ServiceResource\Pages;
+use App\Filament\Resources\ServiceResource\RelationManagers;
+use App\Models\Service;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class ServiceResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationGroup = 'Hizmet Yönetimi';
+    protected static ?string $modelLabel = 'Hizmet';
+    protected static ?int $navigationSort = 2;
+    protected static ?string $navigationIcon = 'heroicon-o-wrench';
+    protected static ?string $activeNavigationIcon = 'heroicon-s-wrench';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('title')
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                        ->label('Başlık'),
+                    TextInput::make('slug')
+                        ->hint('Otomatik Doldurulmaktadır!')
+                        ->label('URL'),
+                ])->columns(2),
+
+                Card::make()->schema([
+                    Forms\Components\TextInput::make('description')
+                        ->label('Açıklama')
+                        ->required()
+                        ->maxLength(140),
+                ])->columns(1),
+
+                Card::make()->schema([
+                    RichEditor::make('body')
+                        ->required()
+                        ->columnSpanFull(),
+                ])->columns(1),
+
+                Card::make()->schema([
+                    SpatieMediaLibraryFileUpload::make('thumbnail')
+                        ->collection('service')
+                        ->imageEditor()
+                        ->imageEditorAspectRatios([null, '16:9', '4:3', '1:1',])
+                        ->label('Resim')
+                        ->columnSpanFull()
+                ])->columns(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Başlık')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->label('URL')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->label('Açıklama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServices::route('/'),
+            'create' => Pages\CreateService::route('/create'),
+            'edit' => Pages\EditService::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Service.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Service extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+
+    protected $fillable = ['title', 'slug', 'description', 'body'];
+
+    public function registerMediaCollections(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('service')
+            ->format(Manipulations::FORMAT_WEBP)
+            ->withResponsiveImages()
+            ->nonQueued();
+    }
+}
