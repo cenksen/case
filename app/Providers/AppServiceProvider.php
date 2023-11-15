6f93d31d0c6806dd28b358e6ce78653ccacbaa68
cenksen <cenksen0605@gmail.com>
2023-11-15 16:34:12 +0300

    Smtp Fix!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,12 @@ use App\Models\Category;
 use App\Models\Page;
 use App\Models\Service;
 use App\Models\Slider;
+use App\Settings\SmtpSettings;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelSettings\Exceptions\MissingSettings;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (! app()->runningInConsole()) {
+            $this->setMailConfig();
             Paginator::useBootstrap();
             View::share('latestBlog', Blog::latest()->limit(3)->get());
             View::share('latestService', Service::latest()->limit(3)->get());
@@ -35,5 +39,27 @@ class AppServiceProvider extends ServiceProvider
             View::share('pages', Page::where('is_active', 1)->get());
 
         }
+    }
+
+    public function setMailConfig()
+    {
+        try {
+            if (Schema::hasTable('settings')) {
+                $settings = app(SmtpSettings::class);
+                $setMail = [
+                    'transport' => 'smtp',
+                    'host' => $settings->smtp_host,
+                    'port' => $settings->smtp_port,
+                    'encryption' => $settings->smtp_password_type,
+                    'username' => $settings->smtp_user_name,
+                    'password' => $settings->smtp_password,
+                    'from' => ['name' => $settings->smtp_email, 'address' => $settings->smtp_email],
+                ];
+                config(['mail.mailers.smtp' => $setMail]);
+            }
+        } catch (MissingSettings $exception) {
+            logger()->warning($exception->getMessage());
+        }
+
     }
 }
